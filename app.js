0b50930bfbef8ce0b7da372d7692331062d22d69
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2024-03-16 00:59:07 +0530

    routes and logger

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-require("dotenv").config({path: "./.env"});
-const express = require('express');
+require("dotenv").config({ path: "./.env" });
+const express = require("express");
 const app = express();
 
-// dbconnection
+// db connection
 require("./models/dbconfig").dbconnection();
 
+// routes
+const userRouter = require("./routes/userRoute");
+
+// logger
+
+const logger = require("morgan");
+app.use(logger("tiny"));
+
+// body pasrser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// http://localhost:8080/api/user/
+// http://localhost:8080/api/user/create
+
+app.use("/api/user", userRouter);
+
+// server
 app.listen(process.env.PORT, () => {
-console.log('the server is running on port ' + process.env.PORT);
+    console.log(`The Server is running on port ${process.env.PORT}`);
 });
 
