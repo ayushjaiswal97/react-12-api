f25fc9e90fb25b390b8875c5816ed1adba566c19
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2024-03-16 01:35:48 +0530

    mongo schema and controller

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,14 +1,10 @@
-
 const express = require("express");
 const router = express.Router();
 
-// /api/user + /
-router.get("/", function (req, res, next) {
-    res.status(200).json({ success: true, message: "This is test route" });
-});
+const { home, createuser } = require("../controllers/userControllers");
 
-router.post("/create", function (req, res, next) {
-    res.status(201).json({ success: true, user: req.body });
-});
+router.get("/", home);
+
+router.post("/create", createuser);
 
 module.exports = router;
